bd00f2cdb53afd361af9dbf87c29f61be49ecd2f
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-09 07:28:18 +0530

    Time: 4 ms (7.69%), Space: 46.3 MB (43.04%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for(int ele : nums){
+            map.put(ele, map.getOrDefault(ele, 0) +1);
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) == 1){
+                if(key % 2 == 0) return key;
+            }
+        }
+        return -1;
+    }
+}

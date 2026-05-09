8c486def343fccff43326c2bdca419ab2b48526d
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-09 06:43:13 +0530

    Time: 1 ms (31.31%), Space: 42.2 MB (79.57%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         while(n > 0){
             if(n % 2 != 0) count++;
-             n = n/2;
+            n = n/2;
         }
         return count;
     }

d5a8ad8f0940352be0f300a6e21f0316089e5215
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-16 13:41:00 +0530

    Time: 5 ms (78.87%), Space: 46.1 MB (17.43%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int rev = 0;
+        int dummy = x;
+        while(x != 0){
+            int rem = x % 10;
+            rev = rev*10 + rem;
+            x = x/10;
+        }
+        if(rev == dummy) return true;
+        return false;
+    }
+}

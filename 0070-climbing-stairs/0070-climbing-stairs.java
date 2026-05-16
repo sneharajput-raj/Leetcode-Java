ad0a2b8765acdabc07551bee1b9c2f7df028f44d
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-16 13:03:17 +0530

    Time: 0 ms (100%), Space: 41.5 MB (97.95%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2) return n;
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+        for(int i=3; i<=n; i++){
+            sum = a+b;
+            a = b;
+            b= sum;
+        }
+        return sum;
+    }
+}

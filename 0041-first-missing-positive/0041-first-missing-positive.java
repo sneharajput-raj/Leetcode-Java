ab3a646eb8623b86ae740f0ccaf6572c159d48ee
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-17 14:13:22 +0530

    Time: 19 ms (10.18%), Space: 93.2 MB (6.81%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,7 +5,7 @@ class Solution {
         for(int ele : nums){
             set.add(ele);
         }
-        for(int i=1; i<=n+1; i++){
+        for(int i=1; i<=n+1; i++){  //positive integer -->means 0 nhi lena h
             if(set.contains(i) == false) return i;
         }
         return -1;

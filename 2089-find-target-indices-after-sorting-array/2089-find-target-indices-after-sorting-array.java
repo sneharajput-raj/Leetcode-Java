46a32fcb13b0a8344b211279e73a0ef81fdb2cb6
Sneha-Rajput <sneharajput7389@gmail.com>
2026-05-24 13:40:51 +0530

    Time: 5 ms (72.29%), Space: 50.4 MB (14.47%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,14 +1,8 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> li = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        // int low = 0;
-        // int high = n-1;
-        ArrayList<Integer> li = new ArrayList<>();
-        // while(low <= high){
-        //     int mid = (low + high)/2;
-        //     if(nums[mid] == target)
-        // }
         for(int i=0; i<n; i++){
             if(nums[i] == target) li.add(i);
         }

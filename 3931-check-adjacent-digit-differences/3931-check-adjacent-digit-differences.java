4e5755662b67138cb023ad19249138aed3221a20
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-01 18:44:17 +0530

    Time: 1 ms (100%), Space: 44.1 MB (11.28%) - LeetHub

--- 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.java
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isAdjacentDiffAtMostTwo(String s) {
+        int n = s.length();
         char[] ch = s.toCharArray();
-        for(int i=0; i<ch.length-1; i++){
-            int a = ch[i] - '0';       //character se number convert --> '1' - '0' = 1
+        for(int i=0; i<n-1; i++){
+            int a = ch[i] - '0';    //character se integer convert ... '1' - '0' = 1
             int b = ch[i+1] - '0';
-            if(Math.abs(a-b) > 2) return false;
+            if(Math.abs(a - b) > 2)  return false;
         }
         return true;
     }

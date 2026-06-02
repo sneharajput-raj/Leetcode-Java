845071ba785f6efa08683ff25da73b80793f603d
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-02 22:36:32 +0530

    Time: 2 ms (91.51%), Space: 47.4 MB (62.03%) - LeetHub

--- 3921-score-validator/3921-score-validator.java
+++ 3921-score-validator/3921-score-validator.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] scoreValidator(String[] events) {
         int n = events.length;
+        int[] ans = new int[2];
         int score = 0;
         int counter = 0;
-        int[] ans = new int[2];
         for(int i=0; i<n; i++){
             String curr = events[i];
             if(curr.equals("W")){
@@ -12,15 +12,13 @@ class Solution {
                     break;
                 }
             }
-            else if(curr.equals("6")) score +=6;
-            else if(curr.equals("4")) score +=4;
-            else if(curr.equals("3")) score +=3;
-            else if(curr.equals("2")) score +=2;
-            else if(curr.equals("1")) score +=1;
-            else if(curr.equals("0")){
-                continue;
-            }
-            else score++;            //'WD' & 'NB'   +1
+            else if(curr.equals("1")) score += 1;
+            else if(curr.equals("2")) score += 2;
+            else if(curr.equals("3")) score += 3;
+            else if(curr.equals("4")) score += 4;
+            else if(curr.equals("6")) score += 6;
+            else if(curr.equals("0")) continue;
+            else score++;       //"WD" or "NB"
         }
         ans[0] = score;
         ans[1] = counter;

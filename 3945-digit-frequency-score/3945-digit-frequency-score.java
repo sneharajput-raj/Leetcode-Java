ee24ce39b3385190eee78358f0055a3e27d4cd14
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-03 19:26:50 +0530

    Time: 0 ms (100%), Space: 42.5 MB (85.86%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int sum = 0;
+        while(n > 0){
+            sum = sum + n % 10;
+            n = n/10;
+        }
+        return sum;
+    }
+}

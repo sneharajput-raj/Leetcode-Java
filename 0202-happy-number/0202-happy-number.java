e925cf9419d95ab5c40aff54ef00a6a7a2c7a990
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-10 20:18:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (87.93%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> set = new HashSet<>();
-        while(n != 1){
-            if(set.contains(n)) return false;
-            set.add(n);
-            int sum = 0;
-            while(n > 0){
-                int rem = n % 10;
-                sum = sum + rem*rem;  //sum of square of digit
-                n = n/10;
-            }
-            n = sum;
+        if(n == 1 || n == 7) return true;
+        if(n==2 || n==3 || n==4 || n==5 || n==6 || n==8 || n==9) return false;
+        int sum = 0;
+        while(n > 0){
+            sum = sum + (n % 10)*(n % 10);
+            n = n/10;
         }
-        return true;
+        return isHappy(sum);
+
     }
 }

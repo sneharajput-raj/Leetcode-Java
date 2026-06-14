a1723ef99084bd8e89c8c81efd46b5f61cb55fdd
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-14 23:01:39 +0530

    Time: 3 ms (25.71%), Space: 80.5 MB (48.95%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -3,11 +3,9 @@ class Solution {
         int i = 0;
         int j = 0;
         while(i < nums1.length && j < nums2.length){
-          if(nums1[i] == nums2[j]){
-            return nums1[i];
-          }
-          else if(nums1[i] < nums2[j]) i++;
-          else j++;
+            if(nums1[i] == nums2[j])  return nums1[i];
+            else if(nums1[i] < nums2[j]) i++;
+            else j++;
         }
         return -1;
     }

42eea2dd7d778358b09dc1eaf094f0f58033d704
Sneha-Rajput <sneharajput7389@gmail.com>
2026-06-29 20:03:59 +0530

    Time: 1 ms (99.96%), Space: 46.2 MB (83.81%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(char[][] board, int row, int col, char ch){
+        //check horizontally
+        for(int j=0; j<9; j++){
+            if(board[row][j] == ch) return false;
+        }
+        //check vertically
+        for(int i=0; i<9; i++){
+            if(board[i][col] == ch) return false;
+        }
+        // now for 3X3 grid
+        int rGrid = row/3 * 3;
+        int cGrid = col/3 * 3;
+        for(int i=rGrid; i<rGrid+3; i++){
+            for(int j=cGrid; j<cGrid+3; j++){
+                if(board[i][j] == ch) return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+                    char ch = board[i][j];
+                    board[i][j] = '.';    //set(.) or delet the current character
+                    if(isValid(board, i, j, ch) == false) return false;
+                    board[i][j] = ch;   //reset
+                }
+            }
+        }
+        return true;
+    }
+}

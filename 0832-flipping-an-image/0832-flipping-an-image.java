8671492c0ad6bb61be793810bf4ea9af6bbf6b8b
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-01 23:03:45 +0530

    Time: 1 ms (44.7%), Space: 45.5 MB (93.75%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image.length;
-        for(int[] row : image){
+        for(int i=0; i<n; i++){
             int left = 0;
             int right = n-1;
-
-            while(left <= right){
-                //if elements are same, they both flip and invert
-                if(row[left] == row[right]){
-                    int newVal = row[left] ^ 1;
-                    row[left] = newVal;
-                    row[right] = newVal;
-                }
-                //if they are different, they remain unchanged after flip + invert
+            while(left < right){
+                //reverse the row using swapping
+                int temp = image[i][left];
+                image[i][left] = image[i][right];
+                image[i][right] = temp;
                 left++;
                 right--;
+            }
+            //invert row using XOR
+            for(int j=0; j<n; j++){
+                image[i][j] = image[i][j] ^ 1;
             }
         }
         return image;

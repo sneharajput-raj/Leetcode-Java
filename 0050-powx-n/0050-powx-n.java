b26a8b1819928a6fa4da5ace5259009ac9c5f5c3
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-03 23:09:29 +0530

    Time: 0 ms (100%), Space: 47.6 MB (79%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,17 @@
 class Solution {
-    public double solve(double x , long n){
-        if(n == 0) return 1;     //base case
-        if(n < 0){     //negative power
-            return solve(1/x , -n);
+    public double power(double x , long n){
+        if(n == 0) return 1;        //base case
+        if(n < 0){                  //negative power
+            return power(1/x , -n);
         }
-        if(n % 2 == 0){      //even
-            return solve(x*x , n/2);
+        if(n % 2 == 0){             //even power
+            return power(x*x , n/2);       
         }
-        else{      //odd
-            return x * solve(x*x , (n-1)/2);
+        else{                       //odd power
+            return x * power(x*x , (n-1)/2);
         }
     }
     public double myPow(double x, int n) {
-        return solve(x, (long)n);     //long --> if n = -31
+        return power(x, (long)n);   //n is long --> if n = 31
     }
 }

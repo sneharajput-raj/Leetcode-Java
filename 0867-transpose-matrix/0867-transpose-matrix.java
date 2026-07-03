f14926c018954722caa964cb731d5308576c825c
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-03 13:46:06 +0530

    Time: 0 ms (100%), Space: 46.7 MB (47%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] num = new int[cols][rows];   //phle cols fill honge phir rows
-        for(int j=0; j<cols; j++){
-            for(int i=0; i<rows; i++){
-                num[j][i] = matrix[i][j];   //row ko col mein transfer
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] ans = new int[col][row];
+        for(int j=0; j<col; j++){
+            for(int i=0; i<row; i++){
+                ans[j][i] = matrix[i][j];
             }
         }
-        return num;
+        return ans;
     }
 }

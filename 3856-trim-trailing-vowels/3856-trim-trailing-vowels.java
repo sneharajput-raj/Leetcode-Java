2ece133d1a8fe26cb9402f18a0cbf01bd6111e19
Sneha-Rajput <sneharajput7389@gmail.com>
2026-07-17 19:44:48 +0530

    Time: 1 ms (99.88%), Space: 44.1 MB (85.04%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -2,11 +2,11 @@ class Solution {
     public String trimTrailingVowels(String s) {
         int count = 0;
         int n = s.length();
+        String vowels = "aeiou";
         for(int i=n-1; i>=0; i--){
             char ch = s.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                count++;
-            }
+            if(vowels.indexOf(ch) != -1) count++;    //mtlb bo vowels string me present hai --> agr present nhi hota hai toh -1 dega
+            // if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')  count++;
             else break;
         }
         return s.substring(0,n-count);

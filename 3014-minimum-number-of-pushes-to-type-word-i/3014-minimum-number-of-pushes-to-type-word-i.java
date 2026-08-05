66f571105298efc77abd4be4afc6e86d4fdc2bed
Sneha-Rajput <sneharajput7389@gmail.com>
2026-08-05 08:21:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (73.49%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,10 +1,14 @@
 class Solution {
     public int minimumPushes(String word) {
-        int n = word.length();
-        int pushes = 0;
-        for(int i=0; i<n; i++){
-            pushes = pushes + (i/8) +1;
-        }
-        return pushes;
+       int n = word.length();
+       int pushes = 0;
+       for(int i=0; i<n; i++){
+        pushes = pushes + (i/8) +1;
+       }
+       return pushes;
     }
 }
+//first 8 letters --> 1 push (can placed as the 1st letter)
+//next 8(9-16) letters --> 2 push
+//next8(17-24) letters --> 3 push
+//remaing letters(25, 26)--> 4 push

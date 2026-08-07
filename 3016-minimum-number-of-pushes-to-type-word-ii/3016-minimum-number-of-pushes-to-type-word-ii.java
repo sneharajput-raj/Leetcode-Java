7d88c3879c3ea49dd712a724d456c7cb8308ee24
Sneha-Rajput <sneharajput7389@gmail.com>
2026-08-07 11:26:30 +0530

    Time: 10 ms (62.85%), Space: 48.1 MB (56.22%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -2,15 +2,15 @@ class Solution {
     public int minimumPushes(String word) {
         int[] freq = new int[26];
         for(char ch : word.toCharArray()){
-            freq[ch - 'a']++;   //convert letter to its zero based index(0-25)
+            freq[ch - 'a']++;
         }
         Arrays.sort(freq);
         int pushes = 0;
         for(int i=0; i<26; i++){
-            int count = freq[25 - i];   //pick the i-th largest frequency
+            int count = freq[25 - i];
             if(count == 0) break;
             int pressCost = (i/8)+1;
-            pushes = pushes + count*pressCost;
+            pushes += count*pressCost;
         }
         return pushes;
     }

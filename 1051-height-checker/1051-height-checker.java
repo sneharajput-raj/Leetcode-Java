f658f64cf673e1ecbf785db7d513bc4134537f25
Sneha-Rajput <sneharajput7389@gmail.com>
2026-08-26 20:27:46 +0530

    Time: 5 ms (37.43%), Space: 43.5 MB (44.81%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] expected = heights.clone();    //clone() -> duplicates the array elements 
+        Arrays.sort(expected);
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(expected[i] != heights[i]) count++;
+        }
+        return count;
+    }
+}

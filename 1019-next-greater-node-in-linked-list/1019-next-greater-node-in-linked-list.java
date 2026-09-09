facd76e3ad2f43d9976b32c318637793b6b8f496
Sneha-Rajput <sneharajput7389@gmail.com>
2026-09-09 13:54:28 +0530

    Time: 239 ms (20.34%), Space: 50.9 MB (5.01%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -9,33 +9,33 @@
  * }
  */
 class Solution {
-    public int count(ListNode head){
+    public int length(ListNode head){
         int c = 0;
-        while(head != null){
+        while(head != null) {
             c++;
             head = head.next;
         }
         return c;
     }
-    public int nextGreater(int i , int x, int[] arr){
+    public int greaterNext(int i, int ele, int[] arr){
         for(int j=i+1; j<arr.length; j++){
-            if(arr[j] > x){
-                return arr[j];
-            }
+            if(arr[j] > ele) return arr[j];
         }
         return 0;
     }
     public int[] nextLargerNodes(ListNode head) {
-        int n = count(head);
+        int n = length(head);
         int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = head.val;
-            head = head.next;
+        ListNode temp = head;
+        int i=0;
+        while(temp != null){
+            arr[i] = temp.val;
+            i++;
+            temp = temp.next;
         }
         int[] ans = new int[n];
-        for(int i=0; i<n; i++){
-            int ele = nextGreater(i, arr[i], arr);
-            ans[i] = ele;
+        for(int k=0; k<n; k++){
+            ans[k] = greaterNext(k, arr[k], arr);
         }
         return ans;
     }
